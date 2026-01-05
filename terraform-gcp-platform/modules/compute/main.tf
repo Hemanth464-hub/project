1c954e583b0ffb1ddbd8b21b8accbdc0da707d0d
Hemanth464-hub <sriyakalamata64@gmail.com>
2026-01-05 10:33:27 -0500

    Update main.tf

--- terraform-gcp-platform/modules/compute/main.tf
+++ terraform-gcp-platform/modules/compute/main.tf
@@ -1,1 +1,21 @@
-resource "google_compute_instance" "vm" { name="vm" machine_type="e2-micro" zone="us-central1-a" boot_disk { initialize_params { image="debian-cloud/debian-11" } } network_interface { network="default" } }
+resource "google_compute_instance" "vm" {
+  name         = "app-vm"
+  machine_type = "e2-micro"
+  zone         = "${var.region}-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = var.network
+    subnetwork = var.subnetwork
+  }
+
+  service_account {
+    email  = var.service_account
+    scopes = ["cloud-platform"]
+  }
+}
