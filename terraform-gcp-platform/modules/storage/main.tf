d6ff5f9e3ff14a3b8b850b6140f4268c8e5239ee
Hemanth464-hub <sriyakalamata64@gmail.com>
2026-01-05 10:33:48 -0500

    Update main.tf

--- terraform-gcp-platform/modules/storage/main.tf
+++ terraform-gcp-platform/modules/storage/main.tf
@@ -1,1 +1,10 @@
-resource "google_storage_bucket" "bucket" { name="example-bucket-12345" location="US" }
+resource "google_storage_bucket" "bucket" {
+  name          = "${var.project_id}-app-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition { age = 30 }
+    action    { type = "Delete" }
+  }
+}
