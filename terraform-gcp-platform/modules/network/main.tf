82c4bdcaa411cf52ea9af91f5281667c3c8f28fb
Hemanth464-hub <sriyakalamata64@gmail.com>
2026-01-05 10:31:46 -0500

    Update main.tf

--- terraform-gcp-platform/modules/network/main.tf
+++ terraform-gcp-platform/modules/network/main.tf
@@ -1,1 +1,11 @@
-resource "google_compute_network" "vpc" { name = "main-vpc" auto_create_subnetworks = false }
+resource "google_compute_network" "vpc" {
+  name                    = "main-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "main-subnet"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
