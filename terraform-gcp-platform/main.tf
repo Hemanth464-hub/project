dff01f4d7cdb900f61416f98ce3f20007d109fda
Hemanth464-hub <sriyakalamata64@gmail.com>
2026-01-05 10:30:58 -0500

    Update main.tf

--- terraform-gcp-platform/main.tf
+++ terraform-gcp-platform/main.tf
@@ -1,5 +1,24 @@
+module "network" {
+  source     = "./modules/network"
+  project_id = var.project_id
+  region     = var.region
+}
 
-module "network" { source = "./modules/network" }
-module "iam" { source = "./modules/iam" }
-module "compute" { source = "./modules/compute" }
-module "storage" { source = "./modules/storage" }
+module "iam" {
+  source     = "./modules/iam"
+  project_id = var.project_id
+}
+
+module "compute" {
+  source     = "./modules/compute"
+  project_id = var.project_id
+  region     = var.region
+  network    = module.network.network_name
+  subnetwork = module.network.subnet_name
+  service_account = module.iam.service_account_email
+}
+
+module "storage" {
+  source     = "./modules/storage"
+  project_id = var.project_id
+}
