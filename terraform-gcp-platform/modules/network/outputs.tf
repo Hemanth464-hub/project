fd8003a53d6bf86865dba796f11b690fb95be5e7
Hemanth464-hub <sriyakalamata64@gmail.com>
2026-01-05 10:32:27 -0500

    Update outputs.tf

--- terraform-gcp-platform/modules/network/outputs.tf
+++ terraform-gcp-platform/modules/network/outputs.tf
@@ -1,1 +1,7 @@
-output "vpc_id" { value = google_compute_network.vpc.id }
+output "network_name" {
+  value = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
+}
